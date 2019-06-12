2ef54f5bfc2b47d349ec912851d883bf0c15d3e1
caelunshun <caelum12321@gmail.com>
2019-06-11 18:46:48 -0600

    Add line break between version and author line in plugin info message

--- src/main/java/me/caelunshun/shun/ShunUtil.java
+++ src/main/java/me/caelunshun/shun/ShunUtil.java
@@ -3,6 +3,7 @@ package me.caelunshun.shun;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
 public class ShunUtil {
   public static void sendMessage(CommandSender sender, String msg) {
@@ -15,5 +16,26 @@ public class ShunUtil {
 
   public static String colorCodes(String input) {
     return ChatColor.translateAlternateColorCodes('&', input);
+  }
+
+  public static void sendPluginInfoMessage(CommandSender sender, Plugin plugin) {
+    sendMessage(
+        sender,
+        String.format(
+            ChatColor.GRAY
+                + "--------"
+                + ChatColor.AQUA
+                + ChatColor.BOLD
+                + plugin.getName()
+                + ChatColor.GRAY
+                + "--------\n"
+                + ChatColor.AQUA
+                + "Version: "
+                + ChatColor.GRAY
+                + plugin.getDescription().getVersion()
+                + ChatColor.AQUA
+                + "\nAuthor: "
+                + ChatColor.GRAY
+                + plugin.getDescription().getAuthors().get(0)));
   }
 }
